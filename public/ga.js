0050f01cf266982bef7fa63f02377267410864ce
Chester Yang <yuh926323@gmail.com>
2026-05-19 17:15:25 +0800

    Add Google Analytics tag

---
+++ public/ga.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || []
+window.gtag = function gtag() {
+  window.dataLayer.push(arguments)
+}
+
+window.gtag('js', new Date())
+window.gtag('config', 'G-685T4FC408')
